e0e670c5ed1b8145849f624b5c27f6cb675c326a
Daniël Lemein <s1185650@student.windesheim.nl>
2023-05-04 14:39:51 +0200

    added new libary to pom.xml made a SerialCommunication for the HMI connection with the robot (not finished). changed the main so you can call it and run it

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,15 @@
-import gui.MainFrame;
+
+import serial.SerialCommunication;
+
+import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) {
-        new MainFrame();
+    public static void main(String[] args) throws IOException, InterruptedException {
+        //new MainFrame();
+        //this is for sending the locations of the products to the robot.
+        SerialCommunication serial = new SerialCommunication("COM3");
+        serial.sendData();
+        //sending product location ends here.
     }
 }
