060f8df4b10f0ef73e146f2cacdba3feedbee3e1
Teun van der Kleij <s1188351@student.windesheim.nl>
2023-05-10 20:10:31 +0200

    Voorraadcode: zoekfunctie geupdate met rode indicatie voor een niet gevonden product

--- src/main/java/gui/controller/Stockcontroller.java
+++ src/main/java/gui/controller/Stockcontroller.java
@@ -5,12 +5,4 @@ import java.awt.*;
 
 public class StockController{
 
-    private final CardLayout layout;
-    private final JPanel root;
-
-    public StockController(CardLayout layout, JPanel root) {
-        this.layout = layout;
-        this.root = root;
-    }
-
 }
