1394aa3007256896269a9c811256a7977080920b
GuusNijhuis <117355326+GuusNijhuis@users.noreply.github.com>
2023-05-12 18:30:48 +0200

    Laden van pakketjes wordt afgehandeld in het HMI doormiddel van Serial communicatie

--- src/main/java/gui/model/RackModel.java
+++ src/main/java/gui/model/RackModel.java
@@ -1,4 +1,35 @@
 package gui.model;
 
-public class RackModel {
+import java.util.ArrayList;
+
+public  class  RackModel {
+    public static ArrayList<PackageModel> rack=new ArrayList<>();
+
+    public static void printRack(){
+        System.out.println("pakketten in rack:");
+        if(rack.size()>0){
+            for(int i = 0; i < rack.size(); i++) {
+                System.out.println(i+": "+rack.get(i));
+            }
+        }
+    }
+    public boolean CheckLocationPossession(int x, int y){
+
+        for (PackageModel item : rack) {
+            if(x==item.getLocationX()&&y==item.getLocationY()){
+                return false;
+            }else{
+                return true;
+            }
+        }
+        return true;
+    }
+
+    public static void addToRack(PackageModel loadmodel){
+        rack.add(loadmodel);
+    }
+    public static void removeFromRack(){
+
+    }
 }
+
